f0883046fe11d2e4e65c1e4172e79e4c1553b5d5
Cory MacDonald <corymacd@netsrv.co>
2021-11-03 15:23:33 -0400

    Update artrunner.ps1
    Added windows tests to the linux tests, so there are tests for each.

--- artrunner.ps1
+++ artrunner.ps1
@@ -2,7 +2,7 @@
 start-transcript transcript.txt
  
 # List of comma seperated values corrosponding to the identifiers here https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/Indexes/Indexes-Markdown/index.md
-$modules_to_run = "T1003.008", "T1003.007", "T1552.003", "T1552.004"
+$modules_to_run = "T1003.002","T1003.003","T1003.008", "T1003.007", "T1552.003", "T1552.004"
 
 # Fetch Atomic Red Team from github
 IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
