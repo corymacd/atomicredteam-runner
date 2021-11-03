dc7200a7c5a1edd24b8144b21fdc4ab47336f6cf
Cory MacDonald <corymacd@netsrv.co>
2021-11-03 15:45:12 -0400

    Update artrunner.ps1

--- artrunner.ps1
+++ artrunner.ps1
@@ -1,13 +1,17 @@
 #start collecting transcript to the relative path (to address cross-platform use of fully qualified paths) 
-start-transcript transcript.txt
+start-transcript -path "./transcript.txt"
+ 
+if($IsWindows = $true) { $PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="C:\AtomicRedTeam\atomics"}}
  
 # List of comma seperated values corrosponding to the identifiers here https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/Indexes/Indexes-Markdown/index.md
 $modules_to_run = "T1003.002","T1003.003","T1003.008", "T1003.007", "T1552.003", "T1552.004"
-
+ 
 # Fetch Atomic Red Team from github
 IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
 Install-AtomicRedTeam -getAtomics -Force
-
-# run the tests from the modules_to_run list
-ForEach($id in $modules_to_run){ invoke-atomictest $id}
  
+# run the tests from the modules_to_run list
+ForEach($id in $modules_to_run){ invoke-atomictest $id -GetPrereqs}
+Write-host "Starting Run"
+ForEach($id in $modules_to_run){ invoke-atomictest $id}
+Write-host "Run Complete."
