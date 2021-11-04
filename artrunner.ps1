045f0c654607ac0204197561a8eda16ea3b0ca01
Cory MacDonald <corymacd@gmail.com>
2021-11-04 17:03:54 -0400

    Updated

--- artrunner.ps1
+++ artrunner.ps1
@@ -1,18 +1,21 @@
 #start collecting transcript to the relative path (to address cross-platform use of fully qualified paths) 
 start-transcript -path "./transcript.txt"
- 
-if($IsWindows = $true) { $PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="C:\AtomicRedTeam\atomics"}}
+
+if($IsWindows -eq $true) { $PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="C:\AtomicRedTeam\atomics"}}
  
 # List of comma seperated values corrosponding to the identifiers here https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/Indexes/Indexes-Markdown/index.md
-$modules_to_run = "T1003.002","T1003.003","T1003.008", "T1003.007", "T1552.003", "T1552.004"
+$modules_to_run = "G0102"
  
-# Fetch Atomic Red Team from github
-IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
-Install-AtomicRedTeam -getAtomics -Force
- 
+function Invoke-ArtTest() {
+    param ($id)
+    
+    write-host "TEST $id - Getting Prereqs..."
+    invoke-atomictest $id -GetPrereqs
+    write-host "TEST $id - Running Test..."
+    invoke-atomictest $id 
+}
+
 # run the tests from the modules_to_run list
-Write-host "Checking/Fetching Prereqs..."
-ForEach($id in $modules_to_run){ invoke-atomictest $id -GetPrereqs}
-Write-host "Starting Run..."
-ForEach($id in $modules_to_run){ invoke-atomictest $id}
-Write-host "Run Complete."
+
+ForEach($id in $modules_to_run){ invoke-arttest $id }
+
